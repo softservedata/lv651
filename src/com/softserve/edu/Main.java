61727cded5cd35df0e5f656c67a75d99f5ae0ccf
Yaroslav <garasym@gmail.com>
2021-11-12 20:01:22 +0200

    template Java project

---
+++ src/com/softserve/edu/Main.java
@@ -0,0 +1,25 @@
+package com.softserve.edu;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        /*-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Hello. What is your name? ");
+        String name = br.readLine();
+        System.out.print("How old are you? ");
+        int age = Integer.parseInt(br.readLine());
+
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age);
+        */
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Hello. What is your name? ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
+        sc.close();
+    }
+}
