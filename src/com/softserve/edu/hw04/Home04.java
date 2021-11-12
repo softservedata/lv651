588b93bca8ac6ab8896028ff2cfacfaac764431c
Yaroslav <garasym@gmail.com>
2021-11-12 20:30:28 +0200

    Template for Homework04

---
+++ src/com/softserve/edu/hw04/Home04.java
@@ -0,0 +1,8 @@
+package com.softserve.edu.hw04;
+
+// Template for Homework 04
+public class Home04 {
+    public static void main(String[] args) {
+        System.out.println("homework");
+    }
+}
