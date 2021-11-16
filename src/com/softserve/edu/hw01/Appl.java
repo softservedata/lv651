3b7c76b437c6b137c46dbe809dd9febdcade64ab
Vlad_Hryshko <vlados0327@gmail.com>
2021-11-16 12:40:40 +0200

    homework 01

--- src/com/softserve/edu/hw01/Appl.java
+++ src/com/softserve/edu/hw01/Appl.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class homework01 {
+public class Appl {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
