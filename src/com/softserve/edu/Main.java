ddbef482c6831826dd718c15cd0d21df715ffffb
Dom28CHuK <andriy28domchuk@gmail.com>
2021-11-16 13:43:40 +0200

    Add Space to massage

--- src/com/softserve/edu/Main.java
+++ src/com/softserve/edu/Main.java
@@ -19,7 +19,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("Hello. What is your name? ");
         String name = sc.nextLine();
-        System.out.println("Hello " + name);
+        System.out.println("Hello  " + name);
         sc.close();
     }
 }
