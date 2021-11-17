c921c67e92121233c65a9bb7a994e4b22af00e90
HrynyshynOleksii <hrunuwun@gmail.com>
2021-11-17 13:22:49 +0200

    add hw02

---
+++ src/com/softserve/edu/hw02/Person.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.softserve.edu.hw02;
+
+/**
+ * @author FizFuck
+ *
+ */
+public class Person {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
