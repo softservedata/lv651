8d267029431baf16e87373a589da1231082c605e
Vova Hordeichuk <vova.gordeichuk@gmail.com>
2021-11-17 12:33:32 +0200

    simple change on homework04

--- src/com/softserve/edu/hw04/Home04.java
+++ src/com/softserve/edu/hw04/Home04.java
@@ -3,6 +3,6 @@ package com.softserve.edu.hw04;
 // Template for Homework 04
 public class Home04 {
     public static void main(String[] args) {
-        System.out.println("homework");
+        System.out.println("homework04");
     }
 }
