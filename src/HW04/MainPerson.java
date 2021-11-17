9a45f1cb09bdee10fecfb5bb0e054ced2e7269bf
ZSergo <zs@gmail.com>
2021-11-18 00:10:51 +0200

    Homework for Les 4.2: Person

---
+++ src/HW04/MainPerson.java
@@ -0,0 +1,22 @@
+package HW04;
+
+public class MainPerson {
+    public static void main(String[] args) {
+
+        Person pMax = new Person("Max", "Fridman");
+        Person pMarta = new Person("Marta", "Simon");
+        //pMax.setBirthYear(1998);
+        pMarta.setBirthYear(2000);
+
+        Person pIvan = new Person("Ivan", "Kozak", 2001);
+        Person pStepan = new Person("Stepan", "Galushka", 2005);
+        Person pTaras = new Person("Taras", "Bulba", 2007);
+
+
+        System.out.println(pMax);
+        System.out.println(pMarta);
+        System.out.println(pIvan);
+        System.out.println(pStepan);
+        System.out.println(pTaras);
+    }
+}
---
+++ src/HW04/Person.java
@@ -0,0 +1,75 @@
+package HW04;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+
+    private static int countPerson;
+
+    public Person(String firstName, String lastName) {
+        countPerson++;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person() {
+        countPerson++;
+        this.firstName = "firstName_" + (countPerson);
+        this.lastName = "lastName_" + (countPerson);
+    }
+
+    public Person(String firstName, String lastName, int birthYear) {
+        countPerson++;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getAge(){
+        return 2021-birthYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                (birthYear == 0 ? "" :", birthYear=" + birthYear) +
+                (birthYear == 0 ? "" : ", age=" + getAge()) +
+                '}';
+    }
+
+    public void changeName(String fn, String ln){
+        setFirstName(fn);
+        setLastName(ln);
+    }
+
+    public void changeName(String fn){
+        setFirstName(fn);
+    }
+}
