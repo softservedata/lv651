4be61f9d7ee6d6db33891383172e6575203a0e2e
Vlad_Hryshko <vlados0327@gmail.com>
2021-11-30 17:34:54 +0200

    Done HomeWork 05

---
+++ src/com/softserve/edu/hw05/Dog.java
@@ -0,0 +1,62 @@
+package com.software.edu.hw05;
+
+public class Dog {
+    private String name, breed;
+    private int age;
+
+    enum breed{
+        BULLDOG,BEAGLE,CAVALIER_KING_CHARLES_SPANIEL;
+    }
+
+    public Dog(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Dog other = (Dog) obj;
+        if (this.name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!this.name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/com/softserve/edu/hw05/Product.java
@@ -0,0 +1,36 @@
+package com.software.edu.hw05;
+
+public class Product {
+    private String name;
+    private int price,quantity;
+
+    public Product(String name, int price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
