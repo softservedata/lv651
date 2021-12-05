189c19f20e44885706fb1708f3a032f5b5a8b938
ZSergo <zs@gmail.com>
2021-12-05 22:41:01 +0200

    Add additional task 5.2 (comb the code)

--- src/HW05/AdditionalTasks/Task2.java
+++ src/HW05/AdditionalTasks/Task2.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 public class Task2 {
     /*2.	Утворити та видрукувати послідовність  , елементи якої  утворюються за наступним правилом:
 
-    */
+     */
     public static void main(String[] args) {
         double[] digs = new double[20];
-        digs[0]=0;
-        digs[1]=5.0/8.0;
+        digs[0] = 0;
+        digs[1] = 5.0 / 8.0;
         for (int i = 2; i < digs.length; i++) {
-            digs[i] = digs[i-1]/2.0 + 3.0/4.0*digs[i-2];
+            digs[i] = digs[i - 1] / 2.0 + 3.0 / 4.0 * digs[i - 2];
         }
         System.out.println(Arrays.toString(digs));
     }
