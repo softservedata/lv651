5d4522b6c6fde95a4f2028edf2eb901cdbab841e
Dom28CHuK <andriy28domchuk@gmail.com>
2021-12-18 14:38:07 +0200

    Add class Car

---
+++ src/com/softserve/edu/homework4/Car.java
@@ -0,0 +1,69 @@
+package com.softserve.edu.homework4;
+
+public class Car {
+    String name;
+    int yerOfProduction;
+    double engineCapacity;
+
+    public Car(String name, int yerOfProduction, double engineCapacity) {
+        this.name = name;
+        this.yerOfProduction = yerOfProduction;
+        this.engineCapacity = engineCapacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYerOfProduction() {
+        return yerOfProduction;
+    }
+
+    public void setYerOfProduction(int yerOfProduction) {
+        this.yerOfProduction = yerOfProduction;
+    }
+
+    public double getEngineCapacity() {
+        return engineCapacity;
+    }
+
+    public void setEngineCapacity(double engineCapacity) {
+        this.engineCapacity = engineCapacity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Car car = (Car) o;
+
+        if (yerOfProduction != car.yerOfProduction) return false;
+        if (Double.compare(car.engineCapacity, engineCapacity) != 0) return false;
+        return name.equals(car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name.hashCode();
+        result = 31 * result + yerOfProduction;
+        temp = Double.doubleToLongBits(engineCapacity);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", yerOfProduction=" + yerOfProduction +
+                ", engineCapacity=" + engineCapacity +
+                '}';
+    }
+}
---
+++ src/com/softserve/edu/homework4/CarManager.java
@@ -0,0 +1,50 @@
+package com.softserve.edu.homework4;
+
+import java.util.Scanner;
+
+public class CarManager {
+    public static void main(String[] args) {
+
+        Car nissan = new Car("Nissan GTR",2008,3.8);
+        Car toyota = new Car("Toyota Supra",2002,2.9);
+        Car mclaren = new Car("McLaren Senna",2018,4.0);
+        Car lexus = new Car("Lexus LFA",2013,4.8);
+
+        Car[] arr = {toyota, nissan, lexus, mclaren};
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the year of production (Available only 2002, 2008, 2013, 2018): ");
+        int numYer = scanner.nextInt();
+
+        for (int i = 0; i < arr.length; i++) {
+            Car ell = arr[i];
+            if (ell.yerOfProduction == numYer) {
+                System.out.println(ell);
+            }
+        }
+
+        /*if (numYer == toyota.yerOfProduction) {
+            System.out.println(arr[0]);
+        } else if (numYer == nissan.yerOfProduction) {
+            System.out.println(arr[1]);
+        } else if (numYer == lexus.yerOfProduction) {
+            System.out.println(arr[2]);
+        } else if (numYer == mclaren.yerOfProduction) {
+            System.out.println(arr[3]);
+        } else {
+            System.out.println("!Not in the database!");
+        } Math.max()*/
+
+        /*if (numYer == 2002) {
+            System.out.println(arr[0]);
+        } else if (numYer == 2008) {
+            System.out.println(arr[1]);
+        } else if (numYer == 2013) {
+            System.out.println(arr[2]);
+        } else if (numYer == 2018) {
+            System.out.println(arr[3]);
+        } else {
+            System.out.println("!Not in the database!");
+        }*/
+    }
+}
