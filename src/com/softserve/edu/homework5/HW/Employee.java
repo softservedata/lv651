41510d2aa75c2a0f4eaf790de17825d76d4056f1
Dom28CHuK <andriy28domchuk@gmail.com>
2021-12-22 12:06:55 +0200

    Add class Employee and Developer

---
+++ src/com/softserve/edu/homework5/HW/Employee.java
@@ -0,0 +1,58 @@
+package com.softserve.edu.homework5.HW;
+
+public class Employee {
+
+    private String name;
+    private int age;
+    private double salary;
+
+    public Employee() {
+        this.name = "";
+        this.age = 0;
+        this.salary = 0;
+    }
+
+    public Employee(String name, int age, double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String report() {
+        String s = "Name: " + name + ", Age: " + age + " years, Salary: " + salary;
+        return s;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public static void main(String[] args) {
+
+        Employee taras = new Employee("Taras", 26, 27500.200);
+        Employee olga = new Developer("Olga", 24, 35500.500, "JAVA Developer");
+
+        System.out.println(taras.report());
+        System.out.println(olga.report());
+    }
+}
