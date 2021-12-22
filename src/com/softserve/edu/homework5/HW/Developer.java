fdccd6b538122762873f3ade132dc7cae4bc1d7b
Dom28CHuK <andriy28domchuk@gmail.com>
2021-12-22 12:08:46 +0200

    Add class Employee and Developer 2.0

--- src/com/softserve/edu/homework5/HW/Developer.java
+++ src/com/softserve/edu/homework5/HW/Developer.java
@@ -4,11 +4,6 @@ public class Developer extends Employee{
 
     private String position;
 
-    public Developer() {
-        super();
-        this.position = "";
-    }
-
     public Developer(String name, int age, double salary, String position) {
         super(name, age, salary);
         this.position = position;
--- src/com/softserve/edu/homework5/HW/Employee.java
+++ src/com/softserve/edu/homework5/HW/Employee.java
@@ -6,12 +6,6 @@ public class Employee {
     private int age;
     private double salary;
 
-    public Employee() {
-        this.name = "";
-        this.age = 0;
-        this.salary = 0;
-    }
-
     public Employee(String name, int age, double salary) {
         this.name = name;
         this.age = age;
@@ -27,24 +21,12 @@ public class Employee {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public double getSalary() {
         return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
     }
 
     public static void main(String[] args) {
