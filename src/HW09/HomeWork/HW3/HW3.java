144328eec8ebdb8cb148b5e688c205e25f0bc184
ZSergo <zs@gmail.com>
2021-12-25 19:54:13 +0200

    Edit homework 9.2 add comparator

---
+++ src/HW09/HomeWork/HW3/HW3.java
@@ -0,0 +1,49 @@
+package HW09.HomeWork.HW3;
+
+import java.util.*;
+
+public class HW3 {
+    /*
+    Write class Student that provides information about the name of the student and his course.
+    Class Student should consist of
+            - properties for access to these fields
+            - constructor with parameters
+            - method printStudents (List students, Integer course), which receives a list of students
+            and the course number and prints to the console the names of the students from the list,
+            which are taught in this course (use an iterator)
+            - methods to compare students by name and by course
+            - In the main() method
+                - declare List students and add to the list five different students
+                - display the list of students ordered by name
+                - display the list of students ordered by course
+     */
+    public static void main(String[] args) {
+
+        ArrayList<Student> uni = new ArrayList<>(
+                Arrays.asList(new Student("Ivan", 1),
+                            new Student("Stepan", 2),
+                            new Student("Roman",2),
+                            new Student("Taras",1),
+                            new Student("Bogdan",3),
+                            new Student("Orest",3)));
+        System.out.println(uni);
+
+        uni.sort((s1, s2) -> s1.getCourse() - s2.getCourse());
+        System.out.println(uni);
+
+//        uni.sort(new Comparator<Student>() {
+//            @Override
+//            public int compare(Student s1, Student s2) {
+//                return s1.getName().toUpperCase().compareTo(s2.getName());
+//            }
+//        });
+        //Sort Example 1
+        //-uni.sort((s1, s2) -> s1.getName().toUpperCase().compareTo(s2.getName()));
+        //Sort Example 2
+
+        Collections.sort(uni,Student.compNameDesc);
+        //System.out.println(uni);
+        Student.printStudents(uni);
+    }
+
+}
---
+++ src/HW09/HomeWork/HW3/Student.java
@@ -0,0 +1,91 @@
+package HW09.HomeWork.HW3;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Objects;
+
+public class Student implements Comparable {
+    public static void printStudents(ArrayList<Student> students) {
+        Iterator i = students.iterator();
+        while (i.hasNext()){
+            Student student = (Student) i.next();
+            System.out.println("Name :" + student.getName() + ", course:" + student.getCourse());
+        }
+    }
+
+    public static Comparator<Student> compNameAsc;
+    public static Comparator<Student> compNameDesc;
+    public static Comparator<Student> compCourseAsc;
+    static {
+        compNameAsc = new Comparator<Student>(){
+            @Override
+            public int compare(Student s1, Student s2){
+                return s1.getName().toUpperCase().compareTo(s2.getName());
+            }
+        };
+
+        compNameDesc = (s1, s2) -> s2.getName().toUpperCase().compareTo(s1.getName());
+
+        compCourseAsc = new Comparator<Student>(){
+            @Override
+            public int compare(Student s1, Student s2){
+                return Integer.compare(s2.getCourse(), s1.getCourse());
+            }
+        };
+    }
+
+
+
+    private String name;
+    private int course;
+
+    public Student(String name, int course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "\nStudent{" +
+                "name='" + name + '\'' +
+                ", course=" + course +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return course == student.course && name.equals(student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, course);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+
+}
