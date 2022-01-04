0bcb39e0e423e184030b805bc5ea46ff8c1e48e8
Nestor <nestor.t721@gmail.com>
2022-01-04 13:34:29 +0200

    Homework08. Task 2

---
+++ src/com/softserve/edu/hw08/task2/Main.java
@@ -0,0 +1,15 @@
+package com.softserve.edu.hw08.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        Wrapper<Integer> wrapper1 = new Wrapper<>();
+        wrapper1.setValue(1);
+        Wrapper<String> wrapper2 = new Wrapper<>();
+        wrapper2.setValue("A");
+        Wrapper<Boolean> wrapper3 = new Wrapper<>();
+        wrapper3.setValue(true);
+        System.out.println(wrapper1.getValue());
+        System.out.println(wrapper2.getValue());
+        System.out.println(wrapper3.getValue());
+    }
+}
---
+++ src/com/softserve/edu/hw08/task2/Wrapper.java
@@ -0,0 +1,13 @@
+package com.softserve.edu.hw08.task2;
+
+public class Wrapper<T> {
+    private T value;
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
