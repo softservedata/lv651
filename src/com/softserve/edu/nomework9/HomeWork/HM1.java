b400dbe67986f8ab23bfc0882b76808faf8ecf13
Dom28CHuK <andriy28domchuk@gmail.com>
2022-02-01 09:54:49 +0200

    Add homework task1

---
+++ src/com/softserve/edu/nomework9/HomeWork/HM1.java
@@ -0,0 +1,25 @@
+package com.softserve.edu.nomework9.HomeWork;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class HM1 {
+    public static void main(String[] args) {
+        System.out.println("Enter in the console sentence of five words: ");
+        List<String> list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            list.add(scanner.nextLine());
+        }
+        System.out.println(list);
+        scanner.close();
+
+        StringBuilder sb = new StringBuilder(list.get(1));
+        sb.reverse();
+        System.out.println(sb.length());
+
+        System.out.println(sb);
+
+    }
+}
