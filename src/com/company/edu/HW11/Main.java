1ca29e939355e3e508f56cfb1cb4c63b7567803b
redminter <oleg.a.fedyshyn@gmail.com>
2022-02-08 18:30:12 +0200

    Homework 11

--- src/com/company/edu/HW11/Main.java
+++ src/com/company/edu/HW11/Main.java
@@ -16,14 +16,14 @@ public class Main {
         List<Integer> numbers = new ArrayList<>();
         System.out.println("Enter first number:");
         numbers.add(HW11.readNumber(1, 100));
-        for(int i=0; i< numbers.size(); i++){
+        for(int i=0; i<9; i++){
             System.out.println("Enter "+(i+2)+" number:");
             numbers.add(HW11.readNumber(numbers.get(i), 100));
         }
-        System.out.println("Output:\n 1<");
+        System.out.print("Output:\n 1<");
         for(int i=0; i< numbers.size(); i++){
-            System.out.println(numbers.get(i)+"<");
+            System.out.print(numbers.get(i)+"<");
         }
-        System.out.println("<100");
+        System.out.println("100");
     }
 }
