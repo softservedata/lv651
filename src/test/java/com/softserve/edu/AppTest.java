d3b23ab7fda2187604a44d8d11ec6786dd61d8ee
Vlad_Hryshko <vlados0327@gmail.com>
2022-02-12 19:34:09 +0200

    updated to JUnit 4.13.1

---
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -0,0 +1,14 @@
+package com.softserve.edu;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testApp ()  {
+        System.out.println("class AppTest, method testApp() ");
+        Assert.assertTrue(true);
+    }
+
+}
