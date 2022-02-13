cd80e1260e01a9b8a0c6ab3c2f4b0acdb955051d
Vlad_Hryshko <vlados0327@gmail.com>
2022-02-13 21:13:42 +0200

    Maven project. Selenium

--- src/test/java/com/softserve/edu/AppTest.java
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -8,6 +8,8 @@ public class AppTest {
     @Test
     public void testApp ()  {
         System.out.println("class AppTest, method testApp() ");
+        System.out.println("***surefire.application.password = "+System.getProperty("surefire.application.password"));
+        System.out.println("***System.getenv().MY_PASSWORD = "+System.getenv().get("MY_PASSWORD"));
         Assert.assertTrue(true);
     }
 
---
+++ src/test/java/com/softserve/edu/First.java
@@ -0,0 +1,12 @@
+package com.softserve.edu;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class First {
+    @Test
+    public void check ()  {
+        System.out.println("class FirstTest, method check() ");
+        Assert.assertTrue(true);
+    }
+}
